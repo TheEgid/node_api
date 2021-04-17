0e641d342404baadbbc2e835ab4592aea459a742
TheEgid <egid.f1@gmail.com>
2021-04-17 15:44:58 +0300

    Rebuild

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ https://ilyachch.gitbook.io/django-rest-framework-russian-documentation
 GraphQL
 
 https://www.twilio.com/blog/graphql-apis-django-graphene
+
+
+https://github.com/Maksim2103/Test_Table/tree/19302038f5fcf203f8da4dc72c4b2b4d492a0019
  
---
+++ react/formatting-project/src/API.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export default class GetRemoteUrl {
+  static async getResource<T>(url: string): Promise<T | null> {
+    const res = await fetch(url);
+    if (!res.ok) {
+      /* eslint-disable no-console */
+      console.log(`Huoston,we have a problem: ${res.status} at ${url}`);
+      /* eslint-enable no-console */
+      return null;
+    }
+    const body = await res.json();
+    return body;
+  }
+}
