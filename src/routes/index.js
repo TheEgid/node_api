4c920f765d747475c4199823ff949921e3d3f94e
TheEgid <egid.f1@gmail.com>
2021-10-24 12:04:55 +0300

    Rebuild

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.json({"message":"Ok"});
+});
+
+export default router;
---
+++ src/routes/users.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+  res.json({"message":"Ok"});
+});
+
+export default router;
